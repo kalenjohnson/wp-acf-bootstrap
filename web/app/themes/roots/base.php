71a010b6f24b11ec3aa24d164308e208bd816c6f
kalenjohnson <info@kalenjohnson.com>
2014-07-04 15:22:42 -0700

    Added new hook for the Bootstrap-ACF plugin

--- web/app/themes/roots/base.php
+++ web/app/themes/roots/base.php
@@ -12,17 +12,20 @@
     get_template_part('templates/header');
   ?>
 
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <main class="main <?php echo roots_main_class(); ?>" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
+  <div class="wrap" role="document">
+    <div class="container">
+      <div class="content row">
+        <main class="main <?php echo roots_main_class(); ?>" role="main">
+          <?php include roots_template_path(); ?>
+        </main><!-- /.main -->
+        <?php if (roots_display_sidebar()) : ?>
+          <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+            <?php include roots_sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+        <?php endif; ?>
+      </div><!-- /.content -->
+    </div><!-- /.container -->
+    <?php do_action('bootstrap-acf'); ?>
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
